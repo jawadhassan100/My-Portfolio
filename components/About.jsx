4429f432325775b3e0a397c02c7a1e3fd0575b44
Jawad Hassan Khan <123384066+jawadhassan100@users.noreply.github.com>
2024-09-30 14:26:30 +0500

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -7,17 +7,18 @@ const About = () => {
       <div className="flex-about">
         <div className="about-text">
           <p>
-            As a developer, I have always been passionate about creating elegant and effective solutions to
-            complex problems. I have a strong foundation in Web development, with a focus on web
-            technologies such as HTML, CSS, and JavaScript. I enjoy working on both the front-end and
-            back-end of applications, and I am always looking for ways to optimize performance, improve user
-            experience, and ensure the highest level of code quality.
+          As a developer, I have always been passionate about creating elegant and effective solutions
+          to complex problems. I have a strong foundation in web development, with a focus on technologies
+          such as Java, Spring Boot, TypeScript ,React ,Node js , Javascript ,Python. I enjoy working on both the front-end and back-end of 
+          applications and am always looking for ways to optimize performance, improve user experience, and 
+          ensure the highest level of code quality.
+        </p>
+          <p>
+            Throughout my career, I have worked on a wide range of projects, from simple static websites to complex
+            enterprise-level applications. I am experienced in working with various development tools and frameworks,
+            including React.js, Next.js, Spring Boot , Node.js, and Django. I am always eager to learn and explore new technologies
+            and constantly seek opportunities to improve my skills and knowledge.
           </p>
-          <p>Throughout my career, I have worked on a wide range of projects, from simple static websites to
-            complex enterprise-level applications. I am experienced in working with a variety of development
-            tools and frameworks, including React.js, Next.js, Node.js, and dajngo. I am always eager
-            to learn and explore new technologies, and I am constantly seeking out opportunities to improve
-            my skills and knowledge.</p>
         </div>
         <div className="about-img">
           <Image src='/images/about.jpeg' className="profile-img" width={300} height={500}/>
